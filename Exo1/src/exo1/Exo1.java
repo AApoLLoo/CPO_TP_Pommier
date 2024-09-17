d7c1e9fe84c296bcf5433514f474036e13a24dd8
Antoine Pommier <antoine.pommier@epfedu.fr>
2024-09-17 16:01:35 +0200

    Création d'un nouveau projet versionné

---
+++ Exo1/src/exo1/Exo1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exo1;
+
+/**
+ * @Auteur  Pommier Antoine!!!!
+ * @Groupe  TDC
+ * @Role du programe Je sais pas
+ * @Date 17/09/2024
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bonjour");
+        System.out.println("Aurevoir");
+    }
+    
+}
