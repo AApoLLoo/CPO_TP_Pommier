339feb94e6bf8b46126b5d5ce46c028da47352ee
Antoine Pommier <antoine.pommier@epfedu.fr>
2024-09-27 14:29:43 +0200

    Tp1 GuessMyNumber Terminé

--- TP1_guessMyNumber_Pommier/src/tp1_guessmynumber_pommier/TP1_guessMyNumber_Pommier.java
+++ TP1_guessMyNumber_Pommier/src/tp1_guessmynumber_pommier/TP1_guessMyNumber_Pommier.java
@@ -6,6 +6,7 @@ package tp1_guessmynumber_pommier;
 
 import java.util.Random;
 import java.util.Scanner;
+
 /**
  * @Auteur Pommier Antoine
  * @Groupe TDC
@@ -18,27 +19,68 @@ public class TP1_guessMyNumber_Pommier {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Décidez d'un niveau de difficulté :" + "/n (1)Facile" + "/n (2)Intermédiaire" + "/n (3)Difficile");
+        int Diff;
+        Diff = sc.nextInt();
         Random generateurAleat = new Random();
         int n;
-        n = generateurAleat.nextInt(100);
-        Scanner sc = new Scanner(System.in);
+        if (Diff == 3){
+            n = generateurAleat.nextInt(500);
+        }else{
+             n = generateurAleat.nextInt(100);
+        }
         int ValeurJoueur;
         System.out.println("Entrer un nombre : ");
         ValeurJoueur = sc.nextInt();
         int Compteur = 0;
         while (n != ValeurJoueur) {
-            if (n<ValeurJoueur) {
-                System.out.println("Trop Grand");
-                ValeurJoueur = sc.nextInt();
-                Compteur += 1;
-            } else { 
-                System.out.println("Trop Petit");
-                ValeurJoueur = sc.nextInt();
-                Compteur += 1;
+            switch (Diff) {
+                case 1:
+                    if (n < ValeurJoueur) {
+                        System.out.println("Trop Grand");
+                        ValeurJoueur = sc.nextInt();
+                        Compteur += 1;
+                    } else if (n < (ValeurJoueur / 2)) {
+                        System.out.println("Vraiment Trop Grand");
+                        ValeurJoueur = sc.nextInt();
+                        Compteur += 1;
+                    } else if (n > (ValeurJoueur)) {
+                        System.out.println("Vraiment Trop Petit");
+                        ValeurJoueur = sc.nextInt();
+                        Compteur += 1;
+                    } else if (n > (ValeurJoueur / 2)) {
+                        System.out.println("Vraiment Trop Petit");
+                        ValeurJoueur = sc.nextInt();
+                        Compteur += 1;
+                    }
+                    break;
+                case 2:
+                    if (n < ValeurJoueur) {
+                        System.out.println("Trop Grand");
+                        ValeurJoueur = sc.nextInt();
+                        Compteur += 1;
+                    } else if (n > (ValeurJoueur)) {
+                        System.out.println("Trop Petit");
+                        ValeurJoueur = sc.nextInt();
+                        Compteur += 1;
+                    }
+                    break;
+                case 3:
+                    if (n < ValeurJoueur) {
+                        System.out.println("Trop Grand");
+                        ValeurJoueur = sc.nextInt();
+                        Compteur += 1;
+                    } else if (n > (ValeurJoueur)) {
+                        System.out.println("Vraiment Trop Petit");
+                        ValeurJoueur = sc.nextInt();
+                        Compteur += 1;
+                    }
+                    break;
+                default:
+                    break;
             }
         }
-        System.out.println("Bravo, vous avez gagne, le nombre a deviner etait : "+n+". Vous avez trouve le nombre apres : "+Compteur+" tentatives");
-        
+        System.out.println("Bravo, vous avez gagne, le nombre a deviner etait : " + n + ". Vous avez trouve le nombre apres : " + Compteur + " tentatives");
     }
-    
 }
