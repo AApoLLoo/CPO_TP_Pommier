01540cb92d82531cec480da99c357e12683a8bc0
Antoine Pommier <antoine.pommier@epfedu.fr>
2024-10-16 12:58:47 +0200

    Test affichage

---
+++ TestAffichage/src/testaffichage/TestAffichage.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testaffichage;
+
+/**
+ *
+ * @author pommi
+ */
+public class TestAffichage {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Interface f = new Interface();
+        f.show();
+    }
+    
+}
